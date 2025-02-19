ae2231360da39993d9407d44f781f096d8583599
Vova Mulyko <Vovamulyko@outlook.com>
2025-02-19 23:19:56 +0200

    Друга лабораторна

---
+++ LAB1/Volt/Volt/Volt/Data/Migrations/20231210140343_ShopVolt.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Volt.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ShopVolt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "ShopItems",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "ShopItems",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
---
+++ LAB1/Volt/Volt/Volt/Models/ShopItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Volt.Models
+{
+    public class ShopItem
+    {
+        public int Id { get; set; } 
+        public Electronic electronic { get; set; }
+
+        public decimal Price {  get; set; }
+        
+        public string ShopCartId { get; set; }
+    }
+}
